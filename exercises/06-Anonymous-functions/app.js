982bd1faa788197335a8e3f6bd18ec9edfcc95e2
Kty <katiuska.rodriguez5@gmail.com>
2021-04-17 10:14:01 +0000

    exercises 6

--- exercises/06-Anonymous-functions/app.js
+++ exercises/06-Anonymous-functions/app.js
@@ -1,6 +1,10 @@
 let multy = function(a,b){
     return a*b;
 }
-//Dont edit anything ABOVE this line
 
+
+//Dont edit anything ABOVE this line
+//var myFirtVar = multy(324234, 47)
 //your code here
+//console.log(myFirtVar)
+console.log(multy(324234, 47))
